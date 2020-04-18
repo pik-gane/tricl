f9fd5691bedb120d04c8cae40af81cb8ab64bc7f
Jobst Heitzig <heitzig@pik-potsdam.de>
2020-04-18 20:47:54 +0200

    test2

--- src/tricl.cpp
+++ src/tricl.cpp
@@ -1,4 +1,4 @@
-/** TriCl, a generic model of social dynamics (!)
+/** TriCl, a generic model of social dynamics (!?)
  *
  * @author Jobst Heitzig, Potsdam Institute for Climate Impact Research, heitzig@pik-potsdam.de
  * @date Oct 18, 2019
